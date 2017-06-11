b1c3ced8a5469c971f86dd531f668e31203fbd7b
uno <st9007a@gmail.com>
2017-06-11 19:45:14 +0800

    refactor op table

--- py/optable.py
+++ py/optable.py
@@ -1,17 +1,17 @@
 class OpTable:
 
     def __init__(self):
-        self.table = {}
+        self.table = []
         with open('./optable.txt') as f:
             lines = f.readlines()
             for line in lines:
                 split = line.split('|')
-                self.table[split[0]] = { 'opcode': split[1], 'oprand_1': split[2], 'oprand_2': split[3] }
+                self.table.append({ 'op': split[0], 'opcode': split[1], 'oprand_1': split[2], 'oprand_2': split[3] })
 
     def search(self, opcode):
         for op in self.table:
-            if self.table[op].opcode == opcode:
-                return self.table[op]
+            if op['opcode'] == opcode:
+                return op
 
         return None
 
