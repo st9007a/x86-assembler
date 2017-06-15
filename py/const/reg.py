514b0f1ad4a97bbc9eecffb2c1537e00260a6068
uno <st9007a@gmail.com>
2017-06-15 14:54:43 +0800

    reg

--- py/const/reg.py
+++ py/const/reg.py
@@ -25,3 +25,27 @@ sreg = {
     'FS': { 'bit': 16, 'value': 4 },
     'GS': { 'bit': 16, 'value': 5 },
 }
+
+r_m_table = {
+    '1': [
+        { 'bit': 0, 'reg': 'BX+SI' },
+        { 'bit': 1, 'reg': 'BX+DI' },
+        { 'bit': 2, 'reg': 'BP+SI' },
+        { 'bit': 3, 'reg': 'BP+DI' },
+        { 'bit': 4, 'reg': 'SI' },
+        { 'bit': 5, 'reg': 'DI' },
+        { 'bit': 7, 'reg': 'BX' },
+    ],
+
+    '2': [
+        { 'bit': 0, 'reg': 'BX+SI' },
+        { 'bit': 1, 'reg': 'BX+DI' },
+        { 'bit': 2, 'reg': 'BP+SI' },
+        { 'bit': 3, 'reg': 'BP+DI' },
+        { 'bit': 4, 'reg': 'SI' },
+        { 'bit': 5, 'reg': 'DI' },
+        { 'bit': 6, 'reg': 'BP' },
+        { 'bit': 7, 'reg': 'BX' },
+    ]
+
+}
